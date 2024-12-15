a4a89138f0aa89b327d7870ff7f35335f7e5ce24
Filipino Bread <Mesa.Francis.Allen.S@gmail.com>
2024-12-15 21:39:38 +0800

    added razor pages

--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TradeTrove.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DatabaseConnection") ?? throw new InvalidOperationException("Connection string 'DatabaseConnection' not found."); ;
+
+builder.Services.AddDbContext<Database>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<TradeTroveUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<Database>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddRazorPages();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,5 +31,5 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
+app.MapRazorPages();
 app.Run();
